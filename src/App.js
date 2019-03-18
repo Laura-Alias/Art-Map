2d78e165a9905b96ec27a7c7a6fbe8011f094d66
Ian Fitzpatrick <ian@cainthus.com>
2019-03-18 11:46:12 +0000

    Using google maps api to add markers to a page

--- src/App.js
+++ src/App.js
@@ -1,54 +1,44 @@
-import React, { Component } from 'react';
-import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-import CurrentLocation from './Map';
+import React, { Component } from "react";
+import { GoogleApiWrapper, Map, InfoWindow, Marker } from "google-maps-react";
+import CurrentLocation from "./Map";
 import "./App.css";
 
-
 export class MapContainer extends Component {
-  onMarkerClick = (props, marker, e) =>
-  this.setState({
-    selectedPlace: props,
-    activeMarker: marker,
-    showingInfoWindow: true
-  });
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: []
+    };
+    this.onMapClicked = this.onMapClicked.bind(this);
+  }
 
-onClose = props => {
-  if (this.state.showingInfoWindow) {
+  onMapClicked(mapProps, map, coord) {
+    const { latLng } = coord;
+    const markersCopy = this.state.markers.slice(0);
+    const newMarker = {
+      position: {
+        lat: latLng.lat(),
+        lng: latLng.lng()
+      }
+    };
+    markersCopy.push(newMarker);
+    console.log(markersCopy);
     this.setState({
-      showingInfoWindow: false,
-      activeMarker: null
+      markers: markersCopy
     });
   }
-};
-
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {}
-  };
 
   render() {
     return (
-      <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
-        <Marker onClick={this.onMarkerClick} name={'You are Here'} />
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow>
-      </CurrentLocation>
+      <Map google={this.props.google} onClick={this.onMapClicked} zoom={14}>
+        {this.state.markers.map(marker => {
+          return <Marker position={marker.position} />;
+        })}
+      </Map>
     );
   }
-
-  
 }
 
-
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAvtForlDhB2fNwIlom0rQk_TBRI0TrGx0'
+  apiKey: "AIzaSyAvtForlDhB2fNwIlom0rQk_TBRI0TrGx0"
 })(MapContainer);
-
