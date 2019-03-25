8deb7b57be027f716cbfb806b2112aa74652204c
Laura Alias <laura.alias.email@gmail.com>
2019-03-25 17:04:11 +0000

    Create marker on my current location

--- src/App.js
+++ src/App.js
@@ -1,54 +1,113 @@
-import React, { Component } from 'react';
-import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-import CurrentLocation from './Map';
+import React, { Component } from "react";
+import { GoogleApiWrapper, Map, InfoWindow, Marker } from "google-maps-react";
 import "./App.css";
 
-
 export class MapContainer extends Component {
-  onMarkerClick = (props, marker, e) =>
-  this.setState({
-    selectedPlace: props,
-    activeMarker: marker,
-    showingInfoWindow: true
-  });
-
-onClose = props => {
-  if (this.state.showingInfoWindow) {
-    this.setState({
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [],
       showingInfoWindow: false,
-      activeMarker: null
+      activeMarker: null,
+      selectedPlace: null,
+      currentLocation: {
+        lat: 53.35014,
+        lng: -6.266155
+      }
+    };
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onClose = this.onClose.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+  }
+
+  onMapClicked(mapProps, map, coord) {
+    const { latLng } = coord;
+    const markersCopy = this.state.markers.slice(0);
+    const newMarker = {
+      position: {
+        lat: latLng.lat(),
+        lng: latLng.lng()
+      }
+    };
+    markersCopy.push(newMarker);
+    this.setState({
+      markers: markersCopy
     });
   }
-};
 
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {}
-  };
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      placeName: props.name,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onClose(props) {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+
+  componentDidMount() {
+    if (navigator && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          const coords = pos.coords;
+          this.setState({
+            currentLocation: {
+              lat: coords.latitude,
+              lng: coords.longitude
+            }
+          });
+        },
+        err => console.log("error with geo location", err)
+      );
+    }
+  }
 
   render() {
     return (
-      <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
-        <Marker onClick={this.onMarkerClick} name={'You are Here'} />
+      <Map
+        google={this.props.google}
+        onClick={this.onMapClicked}
+        zoom={14}
+        initialCenter={this.state.currentLocation}
+      >
+        {/* This marker is for the current location */}
+        <Marker
+          onClick={this.onMarkerClick}
+          position={this.state.currentLocation}
+          name="Current Location"
+        />
+        {/* These are the markers we add when we click on the map*/}
+        {this.state.markers.map((marker, index) => {
+          return (
+            <Marker
+              key={index}
+              onClick={this.onMarkerClick}
+              position={marker.position}
+              name="Place To Discover"
+            />
+          );
+        })}
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
           onClose={this.onClose}
         >
           <div>
-            <h4>{this.state.selectedPlace.name}</h4>
+            <h4>{this.state.placeName ? this.state.placeName : ""}</h4>
           </div>
         </InfoWindow>
-      </CurrentLocation>
+      </Map>
     );
   }
-
-  
 }
 
-
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAvtForlDhB2fNwIlom0rQk_TBRI0TrGx0'
+  apiKey: "AIzaSyAvtForlDhB2fNwIlom0rQk_TBRI0TrGx0"
 })(MapContainer);
-
--- src/Form.js
+++ src/Form.js
@@ -2,18 +2,67 @@ import React from "react";
 import "./App.css";
 
 
-class Gradient extends React.Component {
-    render() {
-        return (
-        <h2> 
-            Form to add a new place 
-        </h2>
-        )
+class Form extends React.Component {
+    constructor() {
+      super()
+  
+      this.state = {
+        name: '',
+        latitude: '',
+        longitude: '',
+      }
+  
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.getMyLocation = this.getMyLocation.bind(this)
     }
-}
+    
+    componentDidMount() {
+      this.getMyLocation()
+    }
+  
+    getMyLocation() {
+      const location = window.navigator && window.navigator.geolocation
+      
+      if (location) {
+        location.getCurrentPosition((position) => {
+          this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          })
+        }, (error) => {
+          this.setState({ latitude: 'err-latitude', longitude: 'err-longitude' })
+        })
+      }
 
+    
 
+    }
+  
+    render() {
+      const { latitude, longitude } = this.state
+      
+      return (
+        <div>
+          <h3>New Graffiti</h3>
+          <form onSubmit={this.handleSubmit} >
+            <div className="form-group">
+             <label for="nameImput">Name</label>
+             <input type="text" name="name" placeholder="Name" />
+            </div>
+            <div className="form-group">
+                <label for="latitudeImput">Latitude</label>
+                <input type="text" value={latitude} />
+            </div>
+            <div className="form-group">
+                <label for="longitudeImput">Longitude</label>
+                <input type="text" value={longitude} />
+          </div>
+          <input type="submit" value="submit" />
+          </form>
+        </div>
+      )
+    }
+  }
 
-export default Gradient
-
+export default Form
 
